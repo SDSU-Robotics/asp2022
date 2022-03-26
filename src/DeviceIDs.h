e9697481a7d17b4bfeb933db8d36bbdca15a1b74
sarahsd0109 <sarah.aman@jacks.sdstae.edu>
2022-03-26 14:36:34 -0500

    Saved

--- src/DeviceIDs.h
+++ src/DeviceIDs.h
@@ -5,7 +5,7 @@ namespace DeviceIDs
 {
 	static const int ExcvDrvLTal = 6; //****
 	static const int ExcvDrvRTal = 13; //****
-	static const int PumpTalon = 8;
+	static const int PumpTalon = 3;
 	static const int canifier = 16;
 	
 
